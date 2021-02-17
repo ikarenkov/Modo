d62751f8bfad572ee4baff9cb04f82e9226b95f7
Konstantin Tskhovrebov <terrakok@gmail.com>
2021-02-17 22:16:27 +0300

    Add required single screen for navigation actions.

--- modo/src/commonMain/kotlin/com/github/terrakok/modo/ModoReducer.kt
+++ modo/src/commonMain/kotlin/com/github/terrakok/modo/ModoReducer.kt
@@ -6,23 +6,23 @@ package com.github.terrakok.modo
 class ModoReducer : NavigationReducer {
     override fun invoke(action: NavigationAction, state: NavigationState): NavigationState =
         when (action) {
-            is Back -> NavigationState(
-                state.chain.dropLast(1)
-            )
             is Forward -> NavigationState(
-                state.chain.plus(action.screen)
+                state.chain + listOf(action.screen, *action.screens)
             )
             is Replace -> NavigationState(
-                state.chain.dropLast(1).plus(action.screen)
+                state.chain.dropLast(1) + listOf(action.screen, *action.screens)
             )
             is NewStack -> NavigationState(
-                action.screen.toList()
+                listOf(action.screen, *action.screens)
             )
             is BackTo -> {
                 val i = state.chain.indexOfLast { it.id == action.screenId }
                 if (i != -1) NavigationState(state.chain.take(i + 1))
                 else state
             }
+            is Back -> NavigationState(
+                state.chain.dropLast(1)
+            )
             is Exit -> NavigationState()
             else -> state
         }
--- modo/src/commonMain/kotlin/com/github/terrakok/modo/NavigationAction.kt
+++ modo/src/commonMain/kotlin/com/github/terrakok/modo/NavigationAction.kt
@@ -5,16 +5,16 @@ package com.github.terrakok.modo
  */
 interface NavigationAction
 
+class Forward(val screen: Screen, vararg val screens: Screen) : NavigationAction
+class Replace(val screen: Screen, vararg val screens: Screen) : NavigationAction
+class NewStack(val screen: Screen, vararg val screens: Screen) : NavigationAction
+class BackTo(val screenId: String) : NavigationAction
 object Back : NavigationAction
 object Exit : NavigationAction
-class Forward(vararg val screen: Screen) : NavigationAction
-class Replace(vararg val screen: Screen) : NavigationAction
-class NewStack(vararg val screen: Screen) : NavigationAction
-class BackTo(val screenId: String) : NavigationAction
 
-fun Modo.back() = dispatch(Back)
-fun Modo.forward(vararg screen: Screen) = dispatch(Forward(*screen))
-fun Modo.replace(vararg screen: Screen) = dispatch(Replace(*screen))
-fun Modo.newStack(vararg screen: Screen) = dispatch(NewStack(*screen))
+fun Modo.forward(screen: Screen, vararg screens: Screen) = dispatch(Forward(screen, *screens))
+fun Modo.replace(screen: Screen, vararg screens: Screen) = dispatch(Replace(screen, *screens))
+fun Modo.newStack(screen: Screen, vararg screens: Screen) = dispatch(NewStack(screen, *screens))
 fun Modo.backTo(screenId: String) = dispatch(BackTo(screenId))
+fun Modo.back() = dispatch(Back)
 fun Modo.exit() = dispatch(Exit)
