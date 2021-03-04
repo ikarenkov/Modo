fc3fd18c40e9984118dab2d149a584dca194c63f
Konstantin Tskhovrebov <terrakok@gmail.com>
2021-03-04 23:57:40 +0300

    Render only last screen.

--- modo-android-compose/src/main/java/com/github/terrakok/modo/android/compose/ComposeRender.kt
+++ modo-android-compose/src/main/java/com/github/terrakok/modo/android/compose/ComposeRender.kt
@@ -1,17 +1,10 @@
 package com.github.terrakok.modo.android.compose
 
 import android.app.Activity
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.graphics.Color
 import com.github.terrakok.modo.NavigationRender
 import com.github.terrakok.modo.NavigationState
 
@@ -31,14 +24,11 @@ class ComposeRender(
 
     @Composable
     fun Content() {
-        state.chain.forEach { screen ->
-                Box(Modifier.fillMaxSize().background(Color.White)) {
-                    Box(Modifier.fillMaxSize().alpha(0f).clickable {})
-                    when (screen) {
-                        is ComposeScreen -> screen.Content()
-                        else -> error("ComposeRender works with ComposeScreen only! Received $screen")
-                    }
-                }
+        state.chain.lastOrNull()?.let { screen ->
+            when (screen) {
+                is ComposeScreen -> screen.Content()
+                else -> error("ComposeRender works with ComposeScreen only! Received $screen")
+            }
         }
     }
 }
