ac4ce2a415d5a2499123ddd7de191b6f9c0c89a2
Konstantin Tskhovrebov <terrakok@gmail.com>
2021-04-01 21:51:38 +0300

    Improve navigation state logging.

--- modo-render-android-fm/src/main/java/com/github/terrakok/modo/android/LogReducer.kt
+++ modo-render-android-fm/src/main/java/com/github/terrakok/modo/android/LogReducer.kt
@@ -1,6 +1,7 @@
 package com.github.terrakok.modo.android
 
 import android.util.Log
+import com.github.terrakok.modo.MultiScreen
 import com.github.terrakok.modo.NavigationAction
 import com.github.terrakok.modo.NavigationReducer
 import com.github.terrakok.modo.NavigationState
@@ -12,7 +13,27 @@ class LogReducer(private val origin: NavigationReducer) : NavigationReducer {
     override fun invoke(action: NavigationAction, state: NavigationState): NavigationState {
         Log.d("Modo", "New action=$action")
         val newState = origin.invoke(action, state)
-        Log.d("Modo", "New state=$newState")
+        val stateStr = "‣root\n${getNavigationStateString("⦙  ", newState).trimEnd()}  ᐊ current screen"
+        Log.d("Modo", "New state=${newState.hashCode()}\n$stateStr")
         return newState
     }
+
+    private fun getNavigationStateString(prefix: String, navigationState: NavigationState): String =
+        navigationState.chain.map { screen ->
+            when (screen) {
+                is MultiScreen -> buildString {
+                    append(prefix)
+                    append('‣')
+                    append(screen.id)
+                    if (screen.stacks.size > 1) {
+                        append(" [${screen.selectedStack + 1}/${screen.stacks.size}]")
+                    }
+                    append('\n')
+                    append(getNavigationStateString("$prefix⦙  ", screen.stacks[screen.selectedStack]))
+                }
+                else -> {
+                    "$prefix${screen.id}\n"
+                }
+            }
+        }.joinToString(separator = "")
 }
