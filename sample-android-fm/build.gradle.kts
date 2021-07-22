c3421e8f483e0bfb0f0701a250ac336d9cef5ea8
aradxxx <aradxxx@gmail.com>
2021-07-22 12:21:44 +0400

    Update dependencies

--- sample-android-fm/build.gradle.kts
+++ sample-android-fm/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 dependencies {
     implementation(project(":modo"))
     implementation(project(":modo-render-android-fm"))
-    implementation("com.google.android.material:material:1.3.0")
-    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
 }
 
