20a516c30a7789c14c5210a856dfc34fc93dde08
Karenkov Igor <karenkovigor@gmail.com>
2022-05-10 14:44:51 +0300

    Add support for custom screen transitions in compose renderer (#15)

--- modo-render-android-compose/src/main/java/com/github/terrakok/modo/android/compose/ComposeRender.kt
+++ modo-render-android-compose/src/main/java/com/github/terrakok/modo/android/compose/ComposeRender.kt
@@ -8,6 +8,24 @@ import com.github.terrakok.modo.NavigationRender
 import com.github.terrakok.modo.NavigationState
 import com.github.terrakok.modo.Screen
 
+typealias RendererContent = @Composable ComposeRendererScope.() -> Unit
+
+val defaultRendererContent: RendererContent = { screen.SaveableContent() }
+
+internal val LocalSaveableStateHolder = staticCompositionLocalOf<SaveableStateHolder?> { null }
+
+@Composable
+fun ComposeScreen.SaveableContent() {
+    LocalSaveableStateHolder.currentOrThrow.SaveableStateProvider(key = screenKey) {
+        Content()
+    }
+}
+
+class ComposeRendererScope(
+    val screen: ComposeScreen,
+    val transitionType: ScreenTransitionType
+)
+
 interface ComposeRenderer : NavigationRender {
 
     val state: State<NavigationState>
@@ -18,36 +36,46 @@ interface ComposeRenderer : NavigationRender {
 }
 
 open class ComposeRenderImpl(
-    private val exitAction: () -> Unit
+    private val exitAction: () -> Unit,
+    private val getTransitionType: (oldScreensStack: List<Screen>, newScreensStack: List<Screen>) -> ScreenTransitionType = ::defaultCalculateTransitionType,
+    private val content: RendererContent = defaultRendererContent
 ) : ComposeRenderer {
-    constructor(activity: Activity) : this({ activity.finish() })
+    constructor(
+        activity: Activity,
+        getTransitionType: (oldScreensStack: List<Screen>, newScreensStack: List<Screen>) -> ScreenTransitionType = ::defaultCalculateTransitionType,
+        content: RendererContent = defaultRendererContent
+    ) : this({ activity.finish() }, getTransitionType, content)
 
     override val state: MutableState<NavigationState> = mutableStateOf(NavigationState())
 
+    private val lastStackEvent: MutableState<ScreenTransitionType> = mutableStateOf(ScreenTransitionType.Idle)
     private val removedScreens = mutableSetOf<Screen>()
 
     override fun invoke(state: NavigationState) {
         if (state.chain.isEmpty()) {
             exitAction()
         }
+        lastStackEvent.value = getTransitionType(this.state.value.chain, state.chain)
         removedScreens.addAll(calculateRemovedScreens(this.state.value.chain, state.chain))
         this.state.value = state
     }
 
     @Composable
     override fun Content() {
-        val stateHolder = rememberSaveableStateHolder()
-        DisposableEffect(key1 = state) {
+        val stateHolder: SaveableStateHolder = rememberSaveableStateHolder()
+        DisposableEffect(key1 = state.value) {
             onDispose {
                 clearStateHolder(stateHolder)
             }
         }
-        state.value.chain.lastOrNull()?.let { screen ->
-            require(screen is ComposeScreen) {
-                "ComposeRender works with ComposeScreen only! Received $screen"
-            }
-            stateHolder.SaveableStateProvider(key = screen.screenKey) {
-                screen.Content()
+        CompositionLocalProvider(
+            LocalSaveableStateHolder provides stateHolder
+        ) {
+            state.value.chain.lastOrNull()?.let { screen ->
+                require(screen is ComposeScreen) {
+                    "ComposeRender works with ComposeScreen only! Received $screen"
+                }
+                ComposeRendererScope(screen, lastStackEvent.value).content()
             }
         }
     }
