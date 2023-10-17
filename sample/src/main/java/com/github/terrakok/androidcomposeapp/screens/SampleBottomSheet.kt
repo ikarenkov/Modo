53a07d2bb57d8e3f2589a9afaf3e31ac242bee50
Evgeny Taskaev <e.taskaev@hh.ru>
2023-10-17 19:33:08 +0300

    bottomsheet animation show and hide after back pressed

--- sample/src/main/java/com/github/terrakok/androidcomposeapp/screens/SampleBottomSheet.kt
+++ sample/src/main/java/com/github/terrakok/androidcomposeapp/screens/SampleBottomSheet.kt
@@ -1,12 +1,18 @@
 package com.github.terrakok.androidcomposeapp.screens
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ModalBottomSheetLayout
 import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.window.DialogProperties
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.repeatOnLifecycle
 import com.github.terrakok.modo.DialogScreen
 import com.github.terrakok.modo.ExperimentalModoApi
 import com.github.terrakok.modo.LocalContainerScreen
@@ -15,6 +21,8 @@ import com.github.terrakok.modo.generateScreenKey
 import com.github.terrakok.modo.stack.StackScreen
 import com.github.terrakok.modo.stack.back
 import com.github.terrakok.modo.util.currentOrThrow
+import kotlinx.coroutines.flow.drop
+import kotlinx.coroutines.launch
 import kotlinx.parcelize.Parcelize
 
 @OptIn(ExperimentalModoApi::class)
@@ -35,13 +43,35 @@ class SampleBottomSheet(
     @OptIn(ExperimentalMaterialApi::class)
     @Composable
     override fun Content() {
+        val lifecycleOwner = LocalLifecycleOwner.current
+        val coroutineScope = rememberCoroutineScope()
         val navigation = LocalContainerScreen.currentOrThrow as StackScreen
-        val state = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.HalfExpanded)
-        LaunchedEffect(key1 = state.currentValue) {
-            if (state.currentValue == ModalBottomSheetValue.Hidden) {
-                navigation.back()
+        val state = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
+
+        LaunchedEffect(key1 = lifecycleOwner) {
+            coroutineScope.launch {
+                lifecycleOwner.repeatOnLifecycle(Lifecycle.State.CREATED) {
+                    state.show()
+                }
             }
         }
+
+        LaunchedEffect(key1 = state) {
+            snapshotFlow { state.currentValue }.drop(1).collect { value ->
+                if (value == ModalBottomSheetValue.Hidden) {
+                    navigation.back()
+                }
+            }
+        }
+
+        BackHandler {
+            coroutineScope.launch {
+                lifecycleOwner.repeatOnLifecycle(Lifecycle.State.CREATED) {
+                    state.hide()
+                }
+            }
+        }
+
         ModalBottomSheetLayout(
             sheetContent = {
                 SampleContent(i, LocalContainerScreen.current as StackScreen, isDialog = false)
