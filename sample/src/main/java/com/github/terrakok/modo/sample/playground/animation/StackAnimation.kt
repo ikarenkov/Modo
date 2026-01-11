0a9aa66ff71284d9e5b081fcd8f2d14240a64804
Karenkov Igor <karenkovigor@gmail.com>
2026-01-11 19:25:23 +0700

    Introduced V2 implementation for predictive back

--- sample/src/main/java/com/github/terrakok/modo/sample/playground/animation/StackAnimation.kt
+++ sample/src/main/java/com/github/terrakok/modo/sample/playground/animation/StackAnimation.kt
@@ -36,6 +36,7 @@ import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
 import logcat.logcat
 
+// FIXME: when navigate forward while predictive back is running there is no animation, just jump to current state
 /**
  * Implementation that relies on rememberAnimationItems and changes input stack.
  * There are no problem when back executed, but there are bugs when back is cancelled several times (blank screen, ignoring input and so on).
@@ -161,6 +162,125 @@ fun StackScreenNew.PredictiveBackStackAnimationPOC(
     }
 
     RenderAnimationItems(modifier, actualItems.value, animator, actualAnimationProgress.value, content)
+}
+
+// FIXME: when navigate forward while predictive back is running there is no animation, just jump to current state
+/**
+ * Implementation that relies on rememberAnimationItems and changes input stack.
+ * There are no problem when back executed, but there are bugs when back is cancelled several times (blank screen, ignoring input and so on).
+ */
+@OptIn(ExperimentalModoApi::class)
+@Composable
+fun StackScreenNew.PredictiveBackStackAnimationPOCV2(
+    modifier: Modifier = Modifier,
+    animator: StackAnimator = fade() + slide(),
+    animationSpec: FiniteAnimationSpec<Float> = tween(durationMillis = 1500),
+    waitForAnimationCompletion: Boolean = true,
+    predictiveBackDesiredStack: (List<Screen>) -> List<Screen> = {
+        it.dropLast(1)
+    },
+    onBack: (() -> Unit)? = {
+        dispatch { StackState(predictiveBackDesiredStack(it.stack)) }
+    },
+    content: @Composable (Screen) -> Unit = { it.SaveableContent(manualResumePause = true) }
+) {
+    val stack = navigationState.stack
+    val predictiveBackDesiredStack: MutableState<List<Screen>?> = remember { mutableStateOf(null) }
+    val predictiveProgressState = remember { mutableFloatStateOf(0f) }
+    val predictiveBackItemsState = remember { mutableStateOf<Map<ScreenKey, AnimationItem>?>(null) }
+
+    var predictiveAnimationJob: Job? by remember { mutableStateOf(null) }
+    val coroutineScope = rememberCoroutineScope()
+    PredictiveBackCallbacks(
+        enabled = stack.size > 1,
+        onBackStarted = {
+            logcat("PredictiveBackCallbacks") { "onBackStarted, $it" }
+            predictiveAnimationJob?.cancel()
+            predictiveBackDesiredStack.value = predictiveBackDesiredStack(navigationState.stack)
+            predictiveBackItemsState.value = predictiveBackDesiredStack.value?.let { newStack ->
+                calculateStackAnimationItems(
+                    oldStack = stack,
+                    stack = newStack
+                )
+            }
+            predictiveProgressState.floatValue = it.progress
+        },
+        onBackProgressed = {
+            predictiveAnimationJob?.cancel()
+            logcat("PredictiveBackCallbacks") { "onBackProgressed, $it" }
+            predictiveProgressState.floatValue = it.progress
+        },
+        onBackPressed = {
+            logcat("PredictiveBackCallbacks") { "onBackPressed" }
+            predictiveAnimationJob = coroutineScope.launch {
+                try {
+                    // Doing back before to let autoanimation recalculate items
+                    onBack?.invoke()
+                    animate(
+                        initialValue = predictiveProgressState.floatValue,
+                        targetValue = 1f,
+                        animationSpec = animationSpec
+                    ) { value, _ ->
+                        predictiveProgressState.floatValue = value
+                    }
+                } finally {
+                    predictiveBackDesiredStack.value = null
+                    predictiveBackItemsState.value = null
+                    // TODO: cancel autoanimation. Test it by running autoanimation for 3s, and this animation for 1 seccond
+                }
+            }
+            // TODO: can simply use predictiveBackDesiredStack to calculete desired idle stack
+//            predictiveBackAnimationItems.value = predictiveBackAnimationItems.value!!.removeExitingAndMarkIdle()
+//            predictiveBackDesiredStack.value = null
+        },
+        onBackCancelled = {
+            logcat("PredictiveBackCallbacks") { "onBackCancelled" }
+            predictiveAnimationJob = coroutineScope.launch {
+                try {
+                    animate(
+                        initialValue = predictiveProgressState.floatValue,
+                        targetValue = 0f,
+                        animationSpec = animationSpec
+                    ) { value, _ ->
+                        logcat("PredictiveBackCallbacks") { "close animation progressed $value" }
+                        predictiveProgressState.floatValue = value
+                    }
+                } catch (e: CancellationException) {
+                    logcat("PredictiveBackCallbacks") { "Animation cancelled" }
+                    throw e
+                } finally {
+                    logcat("PredictiveBackCallbacks") { "Animation finally block - clearing state" }
+                    predictiveProgressState.floatValue = 0f
+//                    predictiveBackDesiredStack.value = null
+//                    predictiveBackItemsState.value = null
+                }
+            }
+        }
+    )
+    // TODO: make a sample of stack where multiple items, stack is Idle and dialog is entering
+
+    LaunchedEffect(predictiveBackItemsState.value) {
+        logcat("StackAnimation") { "predictiveBackAnimationItems: ${predictiveBackItemsState.value}" }
+    }
+
+    val predictiveBackItems = predictiveBackItemsState.value
+    val (screenItems, progress) = if (predictiveBackItems != null) {
+        predictiveBackItems to predictiveProgressState.floatValue
+    } else {
+        // We keep default behavior for case when we are not in predictive back.
+        // Putting it into different if branch also helps to display current state without any animation,
+        // because it resets since it leaves composition.
+        val autoAnimationScreensState = rememberAnimationItems(
+            navigationState.stack,
+            waitForAnimationCompletion = waitForAnimationCompletion,
+        )
+        val animationProgressState = autoLaunchAnimation(
+            animationScreenItems = autoAnimationScreensState,
+            animationSpec = animationSpec,
+        )
+        autoAnimationScreensState.value to animationProgressState.floatValue
+    }
+    RenderAnimationItems(modifier, screenItems, animator, progress, content)
 }
 
 // TODO: make a sample of stack where multiple items, stack is Idle and dialog is entering
